8db97c0ad24909bdb14ffe775ff7c26061324e79
Adriele Rodrigues <drikasera@gmail.com>
2022-03-08 20:59:29 -0300

    calculadora

---
+++ aula 2/script.js
@@ -0,0 +1,14 @@
+//tipos primitivos
+
+//bolean
+var vOUf = false;
+console.log(typeof(vOUf));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'drika';
+console.log(typeof(nome));
+
---
+++ aula 3 array/script.js
@@ -0,0 +1,13 @@
+//o que são vetores ou arrays
+
+//como declarar arrays
+/* let array = ['string',1,true];
+console.log(array); */
+
+//pode guardar vários tipos de dados
+/*let array = ['string',1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array); */
+
+
+let object = {String:'string', number: 1, boolen: true, array: ['array'], objectInterno: {objectInterno: 'objeto interno'}};
+console.log(object.boolen);
